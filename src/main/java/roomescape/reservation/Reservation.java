5ec86491467d495424f681d44851b274f5d7ee5e
RaZel713 <razel7130@gmail.com>
2025-04-20 00:03:07 +0900

    refactor: 예약 정보 입력 값이 null일 경우 구체적인 예외 메시지를 던지도록 수정
    - 예약 생성 시 name, date, time의 null 여부를 검증하고 IllegalArgumentException으로 명확한 예외 메시지 반환하도록 변경한다.

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -2,16 +2,21 @@ package roomescape.reservation;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Objects;
 
 public record Reservation(
         long id, String name, LocalDate date, LocalTime time
 ) {
 
     public Reservation {
-        Objects.requireNonNull(name);
-        Objects.requireNonNull(date);
-        Objects.requireNonNull(time);
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 이름이 비어 있을 수 없습니다.");
+        }
+        if (date == null) {
+            throw new IllegalArgumentException("[ERROR] 날짜가 비어 있을 수 없습니다.");
+        }
+        if (time == null) {
+            throw new IllegalArgumentException("[ERROR] 시간이 비어 있을 수 없습니다.");
+        }
     }
 
     public Reservation writeId(final long id) {
