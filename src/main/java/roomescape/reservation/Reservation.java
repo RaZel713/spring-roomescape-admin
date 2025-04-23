96e01153b6450e00843ff3854e7b0cfb14c8cab0
RaZel713 <razel7130@gmail.com>
2025-04-23 09:38:58 +0900

    refactor: 미사용 메서드 제거

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -18,8 +18,4 @@ public record Reservation(
             throw new IllegalArgumentException("[ERROR] 시간이 비어 있을 수 없습니다.");
         }
     }
-
-    public Reservation writeId(final long id) {
-        return new Reservation(id, name, date, time);
-    }
 }
