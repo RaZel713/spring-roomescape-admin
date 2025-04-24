e1569436b0fe0677029603ee62ca006e5f5b93d9
RaZel713 <razel7130@gmail.com>
2025-04-24 18:02:36 +0900

    refactor: 에러 사인을 상수 처리

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -10,15 +10,17 @@ public record Reservation(
         ReservationTime time
 ) {
 
+    private static final String ERROR_SIGN = "[ERROR] ";
+
     public Reservation {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 이름이 비어 있을 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_SIGN + "이름이 비어 있을 수 없습니다.");
         }
         if (date == null) {
-            throw new IllegalArgumentException("[ERROR] 날짜가 비어 있을 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_SIGN + "날짜가 비어 있을 수 없습니다.");
         }
         if (time == null) {
-            throw new IllegalArgumentException("[ERROR] 시간이 비어 있을 수 없습니다.");
+            throw new IllegalArgumentException(ERROR_SIGN + "시간이 비어 있을 수 없습니다.");
         }
     }
 }
