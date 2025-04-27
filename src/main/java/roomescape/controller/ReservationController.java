0e5973c2b9eaeb8b027ec73aa191f9a10864fc5f
RaZel713 <razel7130@gmail.com>
2025-04-27 22:52:24 +0900

    refactor: RestController를 사용해 컨트롤러 수정
    - @Controller를 @RestController로 변경하여 JSON 응답을 명확히 한다.
    - 메서드는 상태 코드에 따라 ResponseEntity 사용을 유지하거나 ResponseEntity 없이 객체 직접 반환한다.

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -3,18 +3,18 @@ package roomescape.controller;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.DTO.CreateReservationRequest;
 import roomescape.domain.reservation.Reservation;
 import roomescape.service.ReservationService;
 
-@Controller
+@RestController
 @RequestMapping("/reservations")
 public class ReservationController {
 
@@ -25,17 +25,15 @@ public class ReservationController {
     }
 
     @PostMapping
-    public ResponseEntity<Reservation> createReservation(
+    public Reservation createReservation(
             @RequestBody final CreateReservationRequest request
     ) {
-        Reservation insertedReservation = reservationService.createReservation(request);
-        return ResponseEntity.ok(insertedReservation);
+        return reservationService.createReservation(request);
     }
 
     @GetMapping
-    public ResponseEntity<List<Reservation>> readAllReservation() {
-        final List<Reservation> reservations = reservationService.readAllReservations();
-        return ResponseEntity.ok(reservations);
+    public List<Reservation> readAllReservation() {
+        return reservationService.readAllReservations();
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/roomescape/controller/ReservationTimeController.java
+++ src/main/java/roomescape/controller/ReservationTimeController.java
@@ -4,17 +4,17 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.domain.reservationtime.ReservationTime;
 import roomescape.service.ReservationTimeService;
 
-@Controller
+@RestController
 @RequestMapping("/times")
 public class ReservationTimeController {
 
@@ -37,9 +37,8 @@ public class ReservationTimeController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ReservationTime>> readAllTime() {
-        final List<ReservationTime> reservationTimes = reservationTimeService.getAllReservationTime();
-        return ResponseEntity.ok(reservationTimes);
+    public List<ReservationTime> readAllTime() {
+        return reservationTimeService.getAllReservationTime();
     }
 
     @DeleteMapping("/{id}")
